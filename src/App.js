d311259c0c93adaad3211ff436d5e0c93dcb99bc
Homayoun <Homayounmoammadi@gmail.com.com>
2023-03-17 01:01:35 +0330

    Useing Outlet

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
   RouterProvider,
-
   Outlet,
   Navigate,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
 
 function App() {
+  const Layout = () => {
+    return (
+      // <div className={`theme-${darkMode ? "dark" : "light"}`}>
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
   const router = createBrowserRouter([
     {
-      // path: "/",
-      // element: (
-      //   <ProtectedRoute>
-      //     <Layout />
-      //   </ProtectedRoute>
-      // ),
+      path: "/",
+      element: (
+        // <ProtectedRoute>
+          <Layout />
+        // </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
