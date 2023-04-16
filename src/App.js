153b32c6a85a93b67d22a0d3c5489abbb0027775
Homayoun Mohammadi <129702378+CodingCrushi@users.noreply.github.com>
2023-04-17 03:18:25 +0430

    fix some bug

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ import Profile from "./pages/profile/Profile";
 import Navbar from "./components/navbar/Navbar";
 import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
+import "./style.scss"
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext";
 
 function App() {
+  const {currentUser} = useContext(AuthContext); 
+
+  const { darkMode } = useContext(DarkModeContext);
+  
   const Layout = () => {
     return (
-      // <div className={`theme-${darkMode ? "dark" : "light"}`}>
-      <div>
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
@@ -28,13 +35,22 @@ function App() {
       </div>
     );
   };
+   
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
-        // <ProtectedRoute>
+        <ProtectedRoute>
           <Layout />
-        // </ProtectedRoute>
+        </ProtectedRoute>
       ),
       children: [
         {
